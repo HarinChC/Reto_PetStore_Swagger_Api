95137141d87cd89ad7adbc2080ca96ed8d033b5b
Harinchc <95585718+HarinChC@users.noreply.github.com>
2024-09-26 07:16:22 -0500

    Autoalizacion caso consulta

--- Petstore_Swagger/src/test/java/petstore/pets/GetPet_ConsultModificationRunner.java
+++ Petstore_Swagger/src/test/java/petstore/pets/GetPet_ConsultModificationRunner.java
@@ -2,9 +2,9 @@ package petstore.pets;
 
 import com.intuit.karate.junit5.Karate;
 
-public class GetPet_ConsultRunner {
+public class GetPet_ConsultModificationRunner {
     @Karate.Test
     Karate testSample() {
-        return Karate.run("classpath:petstore/pets/get_Pet_Consult.feature");
+        return Karate.run("classpath:petstore/pets/get_Pet_ConsultModification.feature");
     }
 }
